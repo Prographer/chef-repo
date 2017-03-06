a0bbb3d2f9273743ca5c8f50c66c9df73f37e01b
JIHO HEO <sexylion369@gmail.com>
2017-03-06 17:07:11 +0900

    sh추가 및 하둡 설치 추가

---
+++ cookbooks/hadoop/attributes/default.rb
@@ -0,0 +1,3 @@
+default['hadoop.version']   =   '2.7.3'
+default['hadoop.home']      =   '/usr/local/hadoop'
+default['hadoop.hdfs.dir']  =   '/home/hdfs'
--- cookbooks/hadoop/recipes/default.rb
+++ cookbooks/hadoop/recipes/default.rb
@@ -22,18 +22,33 @@ bash 'java_install' do
     curl -LO 'http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.rpm' -H 'Cookie: oraclelicense=accept-securebackup-cookie'
     rpm -i jdk-8u111-linux-x64.rpm
     rm jdk-8u111-linux-x64.rpm
+    rm /usr/bin/java && ln -s /usr/java/default/bin/java /usr/bin/java
+    EOF
+end
+
+# hadoop 설치
+bash 'hadoop_install' do
+  code <<-EOF
+    curl -Ls http://apache.tt.co.kr/hadoop/common/hadoop-#{node['hadoop-version']}/hadoop-#{node['hadoop-version']}.tar.gz | tar -xz -C /usr/local/
+    cd /usr/local && ln -s ./hadoop-$HADOOP_VERSION hadoop
+    cd /usr/local/hadoop && mkdir -p logs
     EOF
 end
 
 ENV['JAVA_HOME'] = '/usr/java/default'
-ENV['PATH'] = "#{ENV['PATH']}:#{ENV['JAVA_HOME']}/bin"
 
-bash 'java_link' do
-  code <<-EOF
-    rm /usr/bin/java && ln -s $JAVA_HOME/bin/java /usr/bin/java
+ENV['HADOOP_HOME']          = '/usr/local/hadoop'
+ENV['HADOOP_PREFIX']        = '/usr/local/hadoop'
+ENV['HADOOP_COMMON_HOME']   = '/usr/local/hadoop'
+ENV['HADOOP_HDFS_HOME']     = '/usr/local/hadoop'
+ENV['HADOOP_MAPRED_HOME']   = '/usr/local/hadoop'
+ENV['HADOOP_YARN_HOME']     = '/usr/local/hadoop'
+ENV['HADOOP_CONF_DIR']      = '/usr/local/hadoop/etc/hadoop'
+ENV['YARN_CONF_DIR']        = "#{ENV['HADOOP_PREFIX']}/etc/hadoop"
 
-    echo $JAVA_HOME
-    echo $PATH
-    
-    EOF
+ENV['PATH'] = "#{ENV['PATH']}:#{ENV['JAVA_HOME']}/bin:#{ENV['HADOOP_PREFIX']}/bin:#{ENV['HADOOP_PREFIX']}/sbin"
+
+bash 'hadoop_env_setting' do
+    sed -i '/^export JAVA_HOME/ s:.*:export JAVA_HOME=/usr/java/default\nexport HADOOP_PREFIX=/usr/local/hadoop\nexport HADOOP_PREFIX=/usr/local/hadoop\n:' /usr/local/hadoop/etc/hadoop/hadoop-env.sh
+    sed -i '/^export HADOOP_CONF_DIR/ s:.*:export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop/:' /usr/local/hadoop/etc/hadoop/hadoop-env.sh
 end
